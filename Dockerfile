a9f1f9bca897084661b9e73e4a1a9e4d02374330
Paul Fanambinantsoa Andriniaina <fanambinantsoa035@gmail.com>
2022-04-14 14:43:18 +0300

    TR0001 : Modification text sur home exemple : 14042022

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 FROM node:12-alpine
 RUN apk add --no-cache python2 g++ make
+RUN apk add git 
+RUN apk add openssh
+
 WORKDIR /app
 
 COPY . .
+RUN rm -Rf /var/www/html/*
+RUN git clone https://github.com/fanambinantsoa/node-docker-app.git
 RUN yarn install --production
 CMD ["node", "src/index.js"]
 EXPOSE 3000
--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Vous n'avez pas encore d'éléments à faire ! Ajoutez-en un ci-dessus !</p>
+                <p className="text-center">Vous n'avez pas encore d'éléments à faire ! Ajoutez-en un ci-dessus ! TEST TEST</p>
             )}
             {items.map(item => (
                 <ItemDisplay
